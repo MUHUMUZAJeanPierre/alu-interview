0c562511825503d04c47ee178ed6ab1dd64b070c
MUHUMUZAJeanPierre <j.muhumuza@alustdudent.com>
2023-09-30 15:16:19 +0000

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,29 +1,31 @@
-ven a list of non-negative integers
-representing the heights of walls with unit width 1
-as if viewing the cross-section of a relief map
-calculate how many square units of water will be retained after it rains.
-"""
+#!/usr/bin/python3
+"""calculate rain"""
 
 
 def rain(walls):
-    """Calculating the square units of retained water"""
-    if not walls:
-        return 0
+    """calculate rain"""
+    total = 0
+    i = 0
+    start = None
+    end = None
+    limit = len(walls)
 
-    n = len(walls)
-    left = [0] * n
-    right = [0] * n
-
-    left[0] = walls[0]
-    for i in range(1, n):
-        left[i] = max(left[i - 1], walls[i])
-
-    right[n - 1] = walls[n - 1]
-    for i in range(n - 2, -1, -1):
-        right[i] = max(right[i + 1], walls[i])
-
-    water = 0
-    for i in range(n):
-        water += min(left[i], right[i]) - walls[i]
-
-    return water
+    while i < limit:
+        last = 0
+        if (i + 1) < (len(walls) - 1):
+            last = walls[i + 1]
+        if start is not None and end is not None:
+            smallest = min([walls[start], walls[end]])
+            for dent in range(start + 1, end):
+                total = total + (smallest - walls[dent])
+            start = None
+            end = None
+        if start is None and walls[i] > 0:
+            start = i
+            i = i + 1
+            continue
+        if start is not None and walls[i] > walls[i - 1] and walls[i] >= last:
+            end = i
+            continue
+        i = i+1
+    return total
