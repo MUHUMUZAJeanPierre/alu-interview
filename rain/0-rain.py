7e8fcfe5b82bc535330bfd0e51f961e1ed4dc075
MUHUMUZAJeanPierre <j.muhumuza@alustdudent.com>
2023-09-30 03:13:28 +0000

    add

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,67 +1,81 @@
 #!/usr/bin/python3
+
 """
-    Given a list of non-negative integers representing the heights of walls
-    with unit width 1,
-    as if viewing the cross-section of a relief map,
-    calculate how many square units of water will be retained after it rains.
+Given a list of non-negative integers representing the heights of walls
+with unit width 1, as if viewing the cross-section of a relief map,
+calculate how many square units of water will be retained after it rains.
+
+Prototype: def rain(walls)
+walls is a list of non-negative integers.
+Return: Integer indicating total amount of rainwater retained.
+Assume that the ends of the list (before index 0 and after index walls[-1]) are
+not walls, meaning they will not retain water.
+If the list is empty return 0.
+
 """
 
 
 def rain(walls):
-    """Defines the rain function"""
-
-    water = 0
-
-    # Check if there are at least three walls or the list is empty
-    if len(walls) < 3 or not walls:
+    """doc comment"""
+    if not walls:
         return 0
 
-    # Iterate over each wall in the middle
-    for i in range(1, len(walls) - 1):
-        # Calculate the maximum height of the wall on the left side
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
 
-        # Calculate the maximum height of the wall on the right side
-        right = walls[i]
-        for j in range(i + 1, len(walls)):
-            right = max(right, walls[j])
-
-        # Calculate the amount of water retained above the current wall
-        water += min(left, right) - walls[i]
+    while left < right:
+        if walls[left] <= walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
 
     return water#!/usr/bin/python3
+
 """
-    Given a list of non-negative integers representing the heights of walls
-    with unit width 1,
-    as if viewing the cross-section of a relief map,
-    calculate how many square units of water will be retained after it rains.
+Given a list of non-negative integers representing the heights of walls
+with unit width 1, as if viewing the cross-section of a relief map,
+calculate how many square units of water will be retained after it rains.
+
+Prototype: def rain(walls)
+walls is a list of non-negative integers.
+Return: Integer indicating total amount of rainwater retained.
+Assume that the ends of the list (before index 0 and after index walls[-1]) are
+not walls, meaning they will not retain water.
+If the list is empty return 0.
+
 """
 
 
 def rain(walls):
-    """Defines the rain function"""
-
-    water = 0
-
-    # Check if there are at least three walls or the list is empty
-    if len(walls) < 3 or not walls:
+    """doc comment"""
+    if not walls:
         return 0
 
-    # Iterate over each wall in the middle
-    for i in range(1, len(walls) - 1):
-        # Calculate the maximum height of the wall on the left side
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
 
-        # Calculate the maximum height of the wall on the right side
-        right = walls[i]
-        for j in range(i + 1, len(walls)):
-            right = max(right, walls[j])
-
-        # Calculate the amount of water retained above the current wall
-        water += min(left, right) - walls[i]
+    while left < right:
+        if walls[left] <= walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
 
     return water
